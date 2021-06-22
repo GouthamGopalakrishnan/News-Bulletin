3c4e35bff31a9ca3cf2db56ef0906a6ab5ad1a3c
GouthamGopalakrishnan <gauthamg1999@gmail.com>
2021-06-22 20:42:29 +0530

    added demo news list

--- src/Components/NewsCard.tsx
+++ src/Components/NewsCard.tsx
@@ -1,52 +1,79 @@
 import React from 'react';
 
 import './NewsCard.css';
-import demo from '../Images/first.jpg';
+import noImage from '../Images/2953962.jpg';
 import userIcon from '../Icons/user.png';
 import timeIcon from '../Icons/date.png';
 
 import { Card,Divider } from 'antd';
 
 
-
+import {demoList} from '../demolist'; //demo list
 
 const NewsCard = () => {
+
+    //convert yyyy-mm-dd to dd-mm-yyyy
+    const format = (inputDate:string) => {
+        var date = new Date(inputDate);
+        if (!isNaN(date.getTime())) {
+            // Months use 0 index.
+            return date.getDate()  + '-' + 0+(date.getMonth()+1) + '-' + date.getFullYear();
+        }
+    }
+
+    
+    const newsArticle=demoList[0].articles.map((article,index) => {
+        //remove the first news
+        if (index !==0) {
+            return(
+            
+                <div className='col-sm-8 col-md-6 col-lg-4 ' style={{ flexDirection: 'column' }}>
+                    <div className="news-card-col">
+                        <Card
+                            className="card-main"
+                            hoverable
+                            style={{ width: '100%', height: '26rem', marginTop: '2em' }}
+                        >
+                            <img src={article.urlToImage === null ? noImage : article.urlToImage} alt="" className='card-img w-100 p-0' />
+                            <div className='card-body'>
+                                <h5 className="card-head-text text-wrap" style={{ height: '3rem' }}>
+                                    {article.title}
+                                </h5>
+                                <p className="card-description text-wrap" style={{ height: '3rem' }}>
+                                    {article.description}
+                                </p>
+                                <div className="post-card-details w-100 pt-0">
+                                    <img src={userIcon} alt="" className="card-user-icon" />
+                                    <p className="card-author-name mt-3 ms-1 text-wrap" style={{ width: '5rem', height: '1rem' }}>{article.author}</p>
+                                    <img src={timeIcon} alt="" className="time-icon ms-3" />
+
+                                    <p className="card-author-name card-publish-date mt-3 ms-1 text-wrap">{format(article.publishedAt.slice(0, 10))}</p>
+                                </div>
+                            </div>
+
+                        </Card>
+                    </div>
+                </div>
+            )
+        }
+
+    })
 
     return(
         <div className='news-card-main'>
             <div className="top-story-div">
                 <h4 className="top-story-text">TOP STORIES</h4>
                 <Divider />
+                
             </div>
-            <div className='justify-content-center'>
+            <div>
                 <div className='row justify-content-center'>
-                    <div className='col-sm-8 col-md-6 col-lg-4 '>
-                    <Card 
-                    className="card-main"
-                    hoverable
-                    style={{ width: '100%'  }}
-                >
-                    <img src={demo} alt="" className='card-img w-100 p-0' />
-                    <div className='card-body'>
-                        <h5 className="card-head-text ">
-                            Apple Podcasts subscriptions launch globally
-                        </h5>
-                        <p className="card-description">
-                            Apple is rolling out paid subscriptions and channels on its Podcasts app..
-                        </p>
-                        <div className="post-details w-100 pt-0">
-                            <img src={userIcon} alt="" className="user-icon"/>
-                            <p className="author-name mt-3 ms-1">Saqib Shah</p>
-                            <img src={timeIcon} alt="" className="time-icon ms-3"/>
-                            <p className="author-name publish-date mt-3 ms-1">16-06-2021</p>
-                        </div>
-                    </div>
+                    
+                    {newsArticle}
 
-                </Card>
                     </div>
                 </div>
 
-            </div>
         </div>
     )
 }
--- src/Components/PrimaryCard.tsx
+++ src/Components/PrimaryCard.tsx
@@ -1,32 +1,41 @@
 import React from 'react';
 
 import './primaryCard.css';
-import image from '../Images/first.jpg';
+// import image from '../Images/first.jpg';
+import noImage from '../Images/2953962.jpg';
+
 import userIcon from '../Icons/user.png';
 import timeIcon from '../Icons/date.png';
+import { demoList } from '../demolist';
 
 
 const PrimaryCard = () => {
+    //convert yyyy-mm-dd to dd-mm-yyyy
+    const format = (inputDate:string) => {
+        var date = new Date(inputDate);
+        if (!isNaN(date.getTime())) {
+            // Months use 0 index.
+            return date.getDate()  + '-' + 0+(date.getMonth()+1) + '-' + date.getFullYear();
+        }
+    }
     return(
         <div className="card-primary-main">
             <div className='row card-primary-row'>
                 <div className=" col-sm-12 col-md-6 card-primary-first-col">
-                    <img src={image} alt="" className="card-primary-image w-100"/>
+                    <img src={demoList[0].articles[0].urlToImage === null ? noImage:demoList[0].articles[0].urlToImage } alt="" className="card-primary-image w-100"/>
                 </div>
                 <div className=" col-sm-12 col-md-6 card-primary-second-col">
                     <h1 className="primary-title">
-                        Apple Podcasts subscriptions launch globally
+                    {demoList[0].articles[0].title}
                     </h1>
                     <p className="primary-description">
-                        Apple has reportedly considered a plan to launch its own 
-                        subscription-based healthcare service, according to a report from 
-                        the Wall Street Journal..
+                    {demoList[0].articles[0].description}
                     </p>
                     <div className="post-details w-100">
                         <img src={userIcon} alt="" className="user-icon"/>
-                        <p className="author-name mt-3 ms-1">Saqib Shah</p>
+                        <p className="author-name mt-3 ms-1">{demoList[0].articles[0].author}</p>
                         <img src={timeIcon} alt="" className="time-icon ms-3"/>
-                        <p className="author-name publish-date mt-3 ms-1">16-06-2021</p>
+                        <p className="author-name publish-date mt-3 ms-1">{format(demoList[0].articles[0].publishedAt.slice(0,10))}</p>
                     </div>
                 </div>
             </div>
